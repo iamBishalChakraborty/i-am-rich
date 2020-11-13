44b2db4c3858760efdc46daacae31e551fed3965
Bishal Chakraborty <cbishal6@gmail.com>
2020-11-13 20:51:53 +0530

    AppBar

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[900],
+        title: Center(
+          child: Text('I am Rich'),
+        ),
+      ),
+    ),
+  ));
+}
